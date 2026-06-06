48e0a7fe8879f4da6f45724de87588d3cd3d012c
Fire <iseulmae0@gmail.com>
2026-06-07 02:25:24 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,343 @@
+
+local indent = 4
+local lower = string.lower
+local byte = string.byte
+local round = math.round
+local running = coroutine.running
+local resume = coroutine.resume
+local status = coroutine.status
+local yield = coroutine.yield
+local create = coroutine.create
+local close = coroutine.close
+local info = debug.info
+local IsA = game.IsA
+local tostring = tostring
+local tonumber = tonumber
+local delay = task.delay
+local spawn = task.spawn
+local clear = table.clear
+local clone = table.clone
+
+local function rawtostring(userdata)
+    if type(userdata) == "table" or typeof(userdata) == "userdata" then
+        local rawmetatable = getrawmetatable(userdata)
+        local cachedstring = rawmetatable and rawget(rawmetatable, "__tostring")
+
+        if cachedstring then
+            local wasreadonly = isreadonly(rawmetatable)
+            if wasreadonly then
+                makewritable(rawmetatable)
+            end
+            rawset(rawmetatable, "__tostring", nil)
+            local safestring = tostring(userdata)
+            rawset(rawmetatable, "__tostring", cachedstring)
+            if wasreadonly then
+                makereadonly(rawmetatable)
+            end
+            return safestring
+        end
+    end
+    return tostring(userdata)
+end
+
+local number_table = {
+    ["inf"] = "math.huge",
+    ["-inf"] = "-math.huge",
+    ["nan"] = "0/0"
+}
+
+local ufunctions
+
+ufunctions = {
+    TweenInfo = function(u)
+        return `TweenInfo.new({u.Time}, {u.EasingStyle}, {u.EasingDirection}, {u.RepeatCount}, {u.Reverses}, {u.DelayTime})`
+    end,
+    Ray = function(u)
+        local Vector3tostring = ufunctions["Vector3"]
+
+        return `Ray.new({Vector3tostring(u.Origin)}, {Vector3tostring(u.Direction)})`
+    end,
+    BrickColor = function(u)
+        return `BrickColor.new({u.Number})`
+    end,
+    NumberRange = function(u)
+        return `NumberRange.new({u.Min}, {u.Max})`
+    end,
+    Region3 = function(u)
+        local center = u.CFrame.Position
+        local centersize = u.Size/2
+        local Vector3tostring = ufunctions["Vector3"]
+
+        return `Region3.new({Vector3tostring(center-centersize)}, {Vector3tostring(center+centersize)})`
+    end,
+    Faces = function(u)
+        local faces = {}
+        if u.Top then
+            table.insert(faces, "Top")
+        end
+        if u.Bottom then
+            table.insert(faces, "Enum.NormalId.Bottom")
+        end
+        if u.Left then
+            table.insert(faces, "Enum.NormalId.Left")
+        end
+        if u.Right then
+            table.insert(faces, "Enum.NormalId.Right")
+        end
+        if u.Back then
+            table.insert(faces, "Enum.NormalId.Back")
+        end
+        if u.Front then
+            table.insert(faces, "Enum.NormalId.Front")
+        end
+        return `Faces.new({table.concat(faces, ", ")})`
+    end,
+    EnumItem = function(u)
+        return tostring(u)
+    end,
+    Enums = function(u)
+        return "Enum"
+    end,
+    Enum = function(u)
+        return `Enum.{u}`
+    end,
+    Vector3 = function(u)
+        return CustomGeneration.Vector3[u] or `Vector3.new({u})`
+    end,
+    Vector2 = function(u)
+        return CustomGeneration.Vector2[u] or `Vector2.new({u})`
+    end,
+    CFrame = function(u)
+        return CustomGeneration.CFrame[u] or `CFrame.new({table.concat({u:GetComponents()},", ")})`
+    end,
+    PathWaypoint = function(u)
+        return `PathWaypoint.new({ufunctions["Vector3"](u.Position)}, {u.Action}, "{u.Label}")`
+    end,
+    UDim = function(u)
+        return `UDim.new({u})`
+    end,
+    UDim2 = function(u)
+        return `UDim2.new({u})`
+    end,
+    Rect = function(u)
+        local Vector2tostring = ufunctions["Vector2"]
+        return `Rect.new({Vector2tostring(u.Min)}, {Vector2tostring(u.Max)})`
+    end,
+    Color3 = function(u)
+        return `Color3.new({u.R}, {u.G}, {u.B})`
+    end,
+    RBXScriptSignal = function(u) -- The server doesnt recive this
+        return "RBXScriptSignal --[[RBXScriptSignal's are not supported]]"
+    end,
+    RBXScriptConnection = function(u) -- The server doesnt recive this
+        return "RBXScriptConnection --[[RBXScriptConnection's are not supported]]"
+    end,
+}
+
+local typeofv2sfunctions = {
+    number = function(v)
+        local number = tostring(v)
+        return number_table[number] or number
+    end,
+    boolean = function(v)
+        return tostring(v)
+    end,
+    string = function(v,l)
+        return formatstr(v, l)
+    end,
+    ["function"] = function(v) -- The server doesnt recive this
+        return f2s(v)
+    end,
+    table = function(v, l, p, n, vtv, i, pt, path, tables, tI)
+        return t2s(v, l, p, n, vtv, i, pt, path, tables, tI)
+    end,
+    Instance = function(v)
+        local DebugId = OldDebugId(v)
+        return i2p(v,generation[DebugId])
+    end,
+    userdata = function(v) -- The server doesnt recive this
+        if configs.advancedinfo then
+            if getrawmetatable(v) then
+                return "newproxy(true)"
+            end
+            return "newproxy(false)"
+        end
+        return "newproxy(true)"
+    end
+}
+
+function t2s(t, l, p, n, vtv, i, pt, path, tables, tI)
+    local globalIndex = table.find(getgenv(), t) -- checks if table is a global
+    if type(globalIndex) == "string" then
+        return globalIndex
+    end
+    if not tI then
+        tI = {0}
+    end
+    if not path then -- sets path to empty string (so it doesn't have to manually provided every time)
+        path = ""
+    end
+    if not l then -- sets the level to 0 (for indentation) and tables for logging tables it already serialized
+        l = 0
+        tables = {}
+    end
+    if not p then -- p is the previous table but doesn't really matter if it's the first
+        p = t
+    end
+    for _, v in next, tables do -- checks if the current table has been serialized before
+        if n and rawequal(v, t) then
+            bottomstr = bottomstr .. "\n" .. rawtostring(n) .. rawtostring(path) .. " = " .. rawtostring(n) .. rawtostring(({v2p(v, p)})[2])
+            return "{} --[[DUPLICATE]]"
+        end
+    end
+    table.insert(tables, t) -- logs table to past tables
+    local s =  "{" -- start of serialization
+    local size = 0
+    l += indent -- set indentation level
+    for k, v in next, t do -- iterates over table
+        size = size + 1 -- changes size for max limit
+        if size > (getgenv().SimpleSpyMaxTableSize or 1000) then
+            s = s .. "\n" .. string.rep(" ", l) .. "-- MAXIMUM TABLE SIZE REACHED, CHANGE 'getgenv().SimpleSpyMaxTableSize' TO ADJUST MAXIMUM SIZE "
+            break
+        end
+        if rawequal(k, t) then -- checks if the table being iterated over is being used as an index within itself (yay, lua)
+            bottomstr ..= `\n{n}{path}[{n}{path}] = {(rawequal(v,k) and `{n}{path}` or v2s(v, l, p, n, vtv, k, t, `{path}[{n}{path}]`, tables))}`
+            --bottomstr = bottomstr .. "\n" .. rawtostring(n) .. rawtostring(path) .. "[" .. rawtostring(n) .. rawtostring(path) .. "]" .. " = " .. (rawequal(v, k) and rawtostring(n) .. rawtostring(path) or v2s(v, l, p, n, vtv, k, t, path .. "[" .. rawtostring(n) .. rawtostring(path) .. "]", tables))
+            size -= 1
+            continue
+        end
+        local currentPath = "" -- initializes the path of 'v' within 't'
+        if type(k) == "string" and k:match("^[%a_]+[%w_]*$") then -- cleanly handles table path generation (for the first half)
+            currentPath = "." .. k
+        else
+            currentPath = "[" .. v2s(k, l, p, n, vtv, k, t, path .. currentPath, tables, tI) .. "]"
+        end
+        if size % 100 == 0 then
+            scheduleWait()
+        end
+        -- actually serializes the member of the table
+        s = s .. "\n" .. string.rep(" ", l) .. "[" .. v2s(k, l, p, n, vtv, k, t, path .. currentPath, tables, tI) .. "] = " .. v2s(v, l, p, n, vtv, k, t, path .. currentPath, tables, tI) .. ","
+    end
+    if #s > 1 then -- removes the last comma because it looks nicer (no way to tell if it's done 'till it's done so...)
+        s = s:sub(1, #s - 1)
+    end
+    if size > 0 then -- cleanly indents the last curly bracket
+        s = s .. "\n" .. string.rep(" ", l - indent)
+    end
+    return s .. "}"
+end
+
+local function isFinished(coroutines: table)
+    for _, v in next, coroutines do
+        if status(v) == "running" then
+            return false
+        end
+    end
+    return true
+end
+
+local specialstrings = {
+    ["\n"] = function(thread,index)
+        resume(thread,index,"\\n")
+    end,
+    ["\t"] = function(thread,index)
+        resume(thread,index,"\\t")
+    end,
+    ["\\"] = function(thread,index)
+        resume(thread,index,"\\\\")
+    end,
+    ['"'] = function(thread,index)
+        resume(thread,index,"\\\"")
+    end
+}
+
+function handlespecials(s, indentation)
+    local i = 0
+    local n = 1
+    local coroutines = {}
+    local coroutineFunc = function(i, r)
+        s = s:sub(0, i - 1) .. r .. s:sub(i + 1, -1)
+    end
+    local timeout = 0
+    repeat
+        i += 1
+        if timeout >= 10 then
+            task.wait()
+            timeout = 0
+        end
+        local char = s:sub(i, i)
+
+        if byte(char) then
+            timeout += 1
+            local c = create(coroutineFunc)
+            table.insert(coroutines, c)
+            local specialfunc = specialstrings[char]
+
+            if specialfunc then
+                specialfunc(c,i)
+                i += 1
+            elseif byte(char) > 126 or byte(char) < 32 then
+                resume(c, i, "\\" .. byte(char))
+                -- s = s:sub(0, i - 1) .. "\\" .. byte(char) .. s:sub(i + 1, -1)
+                i += #rawtostring(byte(char))
+            end
+            if i >= n * 100 then
+                local extra = string.format('" ..\n%s"', string.rep(" ", indentation + indent))
+                s = s:sub(0, i) .. extra .. s:sub(i + 1, -1)
+                i += #extra
+                n += 1
+            end
+        end
+    until char == "" or i > 10000
+    while not isFinished(coroutines) do
+        RunService.Heartbeat:Wait()
+    end
+    clear(coroutines)
+    if i > 10000 then
+        s = string.sub(s, 0, 10000)
+        return s, true
+    end
+    return s, false
+end
+
+function formatstr(s, indentation)
+    if not indentation then
+        indentation = 0
+    end
+    local handled, reachedMax = handlespecials(s, indentation)
+    return '"' .. handled .. '"' .. (reachedMax and " --[[ MAXIMUM STRING SIZE REACHED, CHANGE 'getgenv().SimpleSpyMaxStringSize' TO ADJUST MAXIMUM SIZE ]]" or "")
+end
+
+local typev2sfunctions = {
+    userdata = function(v,vtypeof)
+        if ufunctions[vtypeof] then
+            return ufunctions[vtypeof](v)
+        end
+        return `{vtypeof}({rawtostring(v)}) --[[Generation Failure]]`
+    end,
+    vector = ufunctions["Vector3"]
+}
+
+function v2s(v, l, p, n, vtv, i, pt, path, tables, tI)
+    local vtypeof = typeof(v)
+    local vtypeoffunc = typeofv2sfunctions[vtypeof]
+    local vtypefunc = typev2sfunctions[type(v)]
+    local vtype = type(v)
+    if not tI then
+        tI = {0}
+    else
+        tI[1] += 1
+    end
+
+    if vtypeoffunc then
+        return vtypeoffunc(v, l, p, n, vtv, i, pt, path, tables, tI)
+    elseif vtypefunc then
+        return vtypefunc(v,vtypeof)
+    end
+    return `{vtypeof}({rawtostring(v)}) --[[Generation Failure]]`
+end
+
+return function(value)
+  newValueLol = ("\n%s"):format(v2s(value))
+  print(newValueLol)
+end
